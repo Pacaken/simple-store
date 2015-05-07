496524ce65fa64e9f3d391181cbf31ccb7d2774b
VITALIK21 <qwweasd2112@mail.ru>
2015-05-07 15:33:18 +0300

    Log out

--- page/log_out.php
+++ page/log_out.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vitalik21
- * Date: 05.05.15
- * Time: 18:56
- */ 
+$isCustomerLogin = false;
+if (isset($_SESSION['is_customer_login'])) {
+    $isCustomerLogin = $_SESSION['is_customer_login'];
+}
+
+$headTitle = 'Log out';
+$contentViewType = '2l';
+//prepare path to block with registration form
+
+$leftMenu = '';
+$contentTemplatePath = generateBlockTemplatePath('cms/log_out.phtml');
+if ($isCustomerLogin) {
+    $_SESSION['is_customer_login'] = false;
+    $logOutResult = "You're out";
+}
+
+//add page.phtml
+require_once generateBlockTemplatePath('page.phtml');
